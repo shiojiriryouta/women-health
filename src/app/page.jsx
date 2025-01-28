3a367566e5cbf69fb8acecaf150f5f8e2a6f9fe5
shiojiriryouta <ojiri.pikachu@gmail.com>
2025-01-28 23:35:08 +0900

    サバ定食

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -306,7 +306,7 @@ export default function Home() {
                     onChange={(e) => setNote(e.target.value)}
                     className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                     rows="1"
-                    placeholder="例：焼き鮭とごはん"
+                    placeholder="例：サバ定食"
                     required
                   ></textarea>
                 </div>
