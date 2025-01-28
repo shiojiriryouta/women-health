a72525632fb6c9bacc432013926e8df84430f8e4
shiojiriryouta <ojiri.pikachu@gmail.com>
2025-01-28 23:45:40 +0900

    保存ボタンのレイアウト変更

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -326,7 +326,7 @@ export default function Home() {
                 <div>
                   <button
                     type="submit"
-                    className="w-full bg-blue-500 text-white py-2 px-4 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
                     保存
                   </button>
